90a000946c4a965400cd5b9bacca1c57db624c90
Justnoones <yanw97841@gmail.com>
2023-08-28 12:53:52 +0630

    react-transition-group

--- src/components/BookLists.jsx
+++ src/components/BookLists.jsx
@@ -5,6 +5,6 @@ export default function BookLists () {
   return (
     <div className="grid md:grid-cols-4 grid-cols-2 gap-4 my-3">
         {[1,2, 3, 4].map(_ => ( <BookList key={_} />) )}
-      </div>
+    </div>
   )
 }
--- src/pages/Layouts/Layout.jsx
+++ src/pages/Layouts/Layout.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../../components/Navbar'
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import './Layout.css';
 
 export default function Layout () {
+  const location = useLocation();
   return (
     <div>
         <Navbar />
-        <div className='max-w-6xl mx-auto p-3'>
-            <Outlet />
-        </div>
+        <SwitchTransition>
+          <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+            <div className='max-w-6xl mx-auto p-3'>
+              <Outlet />
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
     </div>
   )
 }
